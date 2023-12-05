ceb7a45be29a00d34f6c9a85e657f4263067e357
Romy KLK <romyklk1610@gmail.com>
2023-12-05 14:13:41 +0100

    📦 NEW:

--- SQL/bibliotheque/bibliotheque.sql
+++ SQL/bibliotheque/bibliotheque.sql
@@ -41,3 +41,13 @@ CREATE TABLE emprunt(
     FOREIGN KEY (id_abonne) REFERENCES abonne(id_abonne),
     FOREIGN KEY (id_livre) REFERENCES livre(id_livre)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO emprunt (id_emprunt, id_livre, id_abonne, date_sortie, date_rendu) VALUES
+(1, 100, 1, '2016-12-07', '2016-12-11'),
+(2, 101, 2, '2016-12-07', '2016-12-18'),
+(3, 100, 3, '2016-12-11', '2016-12-19'),
+(4, 103, 4, '2016-12-12', '2016-12-22'),
+(5, 104, 1, '2016-12-15', '2016-12-30'),
+(6, 105, 2, '2017-01-02', '2017-01-15'),
+(7, 105, 3, '2017-02-15', NULL),
+(8, 100, 2, '2017-02-20', NULL);

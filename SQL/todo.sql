fddc57effe2bd5208f9fa93b54d8f7fc85065ad2
Romy KLK <romyklk1610@gmail.com>
2023-12-05 11:48:45 +0100

    📦 NEW:

--- SQL/todo.sql
+++ SQL/todo.sql
@@ -1,6 +1,6 @@
--- SQLBook: Code
 -- 1 -- Afficher la profession de l'employé 547.
-	/* SELECT seervice from employes WHERE id_employes=547;
+	SELECT service FROM employes WHERE id_employes = 547;
+	/*
 	+------------+
 	| service    |
 	+------------+
@@ -8,8 +8,10 @@
 	+------------+
 	*/
 
+
 -- 2 -- Afficher la date d'embauche d'Amandine.
-	/* SELECT date_embauche from employes WHERE prenom='Amandine';
+	SELECT date_embauche FROM employes WHERE prenom = 'Amandine';
+	/* 
 	+---------------+
 	| date_embauche |
 	+---------------+
@@ -18,7 +20,8 @@
 	 */
 
 -- 3 -- Afficher le nom de famille de Guillaume
-	/* SELECT nom from employes WHERE prenom='Guillaume';
+	SELECT nom FROM employes WHERE prenom = 'Guillaume';
+	/* 
 	+--------+
 	| nom    |
 	+--------+
@@ -27,6 +30,7 @@
 	 */
 		
 -- 4 -- Afficher le nombre de personne ayant un n° id_employes commençant par le chiffre 5.
+	SELECT COUNT(*) FROM employes WHERE id_employes LIKE '5%';
 	/* 
 	+----------+
 	| COUNT(*) |
@@ -34,8 +38,9 @@
 	|        3 |
 	+----------+
 	 */
-		
+
 -- 5 -- Afficher le nombre de commerciaux.
+	SELECT COUNT(*) AS nombre FROM employes WHERE service = 'commercial';
 
 	/* 
 	+--------+
@@ -45,6 +50,7 @@
 	 */
 
 -- 6 -- Afficher le salaire moyen des informaticiens (+arrondie).
+	SELECT round(AVG( salaire )) FROM employes WHERE service = 'informatique';
 	/* 
 	+-----------------------+
 	| round(AVG( salaire )) |
@@ -52,8 +58,9 @@
 	|                  2617 |
 	+-----------------------+
 	 */
-		
+
 -- 7 -- Afficher les 5 premiers employés après avoir classer leur nom de famille par ordre alphabétique.
+	SELECT * FROM employes ORDER BY nom ASC LIMIT 5;
 	/* 
 	+-------------+---------+----------+------+--------------+---------------+---------+
 	| id_employes | prenom  | nom      | sexe | service      | date_embauche | salaire |
@@ -67,6 +74,7 @@
 	 */
 		
 -- 8 -- Afficher le coût des commerciaux sur 1 année.
+	SELECT SUM(salaire*12) FROM employes WHERE service = 'commercial';
 	/* 
 	+-----------------+
 	| SUM(salaire*12) |
@@ -76,6 +84,7 @@
 	 */
 
 -- 9 -- Afficher le salaire moyen par service. (service + salaire moyen)
+	SELECT service, round(AVG( salaire )) FROM employes GROUP BY service;
 	/* 
 	+---------------+-----------------------+
 	| service       | round(AVG( salaire )) |
@@ -93,7 +102,8 @@
 	 */
 		
 -- 10 -- Afficher le nombre de recrutement sur l'année 2010 (+alias).
-
+	SELECT COUNT(*) AS 'nb de recrutement' FROM employes WHERE date_embauche LIKE '2010%';
+	SELECT COUNT(*) AS 'nb de recrutement' FROM employes WHERE date_embauche BETWEEN '2010-01-01' AND '2010-12-31';
 	/* 
 	+-------------------+
 	| nb de recrutement |
@@ -103,6 +113,7 @@
 	 */
 		
 -- 11 -- Afficher le salaire moyen appliqué lors des recrutements sur la période allant de 2012 a 2014
+	SELECT AVG(salaire) FROM employes WHERE date_embauche BETWEEN '2012-01-01' AND '2014-12-31';
 
 	/* 
 	+--------------+
@@ -113,7 +124,7 @@
 	 */
 
 -- 12 -- Afficher le nombre de service différent
-
+SELECT COUNT(DISTINCT(service)) FROM employes;
 	/* 
 	+--------------------------+
 	| COUNT(DISTINCT(service)) |
@@ -123,7 +134,9 @@
 	 */
 
 -- 13 -- Afficher tous les employés (sauf ceux du service production et secrétariat)
+	SELECT nom,prenom FROM employes WHERE service != 'production' AND service != 'secretariat';
 
+	SELECT nom,prenom FROM employes WHERE service NOT IN ('production','secretariat');
 	/* 
 	+----------+-------------+
 	| nom      | prenom      |
@@ -147,7 +160,7 @@
 	 */
 
 -- 14 -- Afficher conjoitement le nombre d'homme et de femme dans l'entreprise
-
+	SELECT sexe , COUNT(*) FROM employes GROUP BY sexe;
 	/* 
 	+------+----------+
 	| sexe | COUNT(*) |
@@ -158,6 +171,7 @@
 	 */
 
 -- 15 -- Afficher les commerciaux ayant été recrutés avant 2015 de sexe masculin et gagnant un salaire supérieur a 2500 €
+	SELECT nom,prenom FROM employes WHERE service = 'commercial' AND date_embauche < '2015-01-01' AND sexe = 'm' AND salaire > 2500;
 	/* 
 	+--------+--------+
 	| nom    | prenom |
@@ -168,7 +182,9 @@
 
 
 -- 16 -- Qui a été embauché en dernier
-	
+	SELECT * FROM employes ORDER BY date_embauche DESC LIMIT 1;
+	SELECT * FROM employes WHERE id_employes = (SELECT MAX(id_employes) FROM employes);
+	SELECT * FROM employes ORDER BY id_employes DESC LIMIT 1;
 	/* 
 	+-------------+-----------+--------+------+-----------+---------------+---------+
 	| id_employes | prenom    | nom    | sexe | service   | date_embauche | salaire |
@@ -178,6 +194,8 @@
 	 */
 
 -- 17 -- Afficher les informations sur l'employé du service commercial gagnant le salaire le plus élevé
+SELECT * FROM employes WHERE service = 'commercial' ORDER BY salaire DESC LIMIT 1;
+SELECT * FROM employes WHERE service='commercial' AND salaire = (SELECT MAX(salaire) FROM employes WHERE service = 'commercial');
 
 	/* 
 	+-------------+--------+--------+------+------------+---------------+---------+
@@ -189,6 +207,7 @@
 
 
 -- 18 -- Afficher le prénom du comptable gagnant le meilleur salaire
+SELECT prenom FROM employes WHERE salaire = (SELECT MAX(salaire) FROM employes WHERE service = 'comptabilite');
 		/* 
 	+---------+
 	| prenom  |
@@ -198,6 +217,10 @@
 	 */
 
 -- 19 -- Afficher le prénom de l'informaticien ayant été recruté en premier
+SELECT * FROM employes WHERE date_embauche = (SELECT MIN(date_embauche) FROM employes WHERE service = 'informatique');
+
+SELECT prenom FROM employes WHERE service = 'informatique' ORDER BY date_embauche  LIMIT 1;
+
 	/* 
 	+-------------+---------+--------+------+--------------+---------------+---------+
 	| id_employes | prenom  | nom    | sexe | service      | date_embauche | salaire |
@@ -207,5 +230,7 @@
 	 */
 
 -- 20 -- Augmenter chaque employé de 100 €
+UPDATE employes SET salaire = salaire + 100;
 
 -- 21 -- Supprimer les employés du service secrétariat
+DELETE FROM employes WHERE service ='secretariat';

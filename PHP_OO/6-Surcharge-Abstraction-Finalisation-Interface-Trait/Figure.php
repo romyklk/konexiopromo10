479e28ba79bb124bdc4455208b3bb6e8ac50962e
Romy KLK <romyklk1610@gmail.com>
2024-01-05 12:59:17 +0100

    📦 NEW:

--- PHP_OO/6-Surcharge-Abstraction-Finalisation-Interface-Trait/Figure.php
+++ PHP_OO/6-Surcharge-Abstraction-Finalisation-Interface-Trait/Figure.php
@@ -34,3 +34,84 @@ Exercice : Cet exercice est un exercice de synthèse qui combine les notions de 
 NB : Pour la formule de la surface du cercle, vous pouvez utiliser la fonction prédéfinie `pi()` qui retourne la valeur de pi.
 Arrondissez le résultat à 3 chiffres après la virgule en utilisant la fonction prédéfinie `round()` dans le cas du périmètre et de la surface du cercle.
 */
+
+interface Forme 
+{
+    public function calculerPerimetre();
+    public function calculerSurface();
+}
+
+trait Affichage
+{
+    public function afficherInformations()
+    {
+        echo 'Informations de la forme : ' . get_class($this) . '<br>';
+        echo 'Périmètre : ' . $this->calculerPerimetre() . 'm<br>';
+        echo 'Surface : ' . $this->calculerSurface() . 'm²<br>';
+    }
+}
+
+abstract class Figure implements Forme 
+{
+    use Affichage;
+
+    public function finaliser()
+    {
+        echo 'La figure est finalisée et ne peut être modifiée.<br>';
+    }
+}
+
+class Rectangle extends Figure
+{
+    private $longueur;
+    private $largeur;
+
+    public function __construct($longueur, $largeur)
+    {
+        $this->longueur = $longueur;
+        $this->largeur = $largeur;
+    }
+
+    public function calculerPerimetre()
+    {
+        return round(2 * ($this->longueur + $this->largeur), 3);
+    }
+
+    public function calculerSurface()
+    {
+        return round($this->longueur * $this->largeur, 3);
+    }
+}
+
+
+class Cercle extends Figure
+{
+    private $rayon;
+
+    public function __construct($rayon)
+    {
+        $this->rayon = $rayon;
+    }
+
+    public function calculerPerimetre()
+    {
+        return round(2 * pi() * $this->rayon, 3);
+    }
+
+    public function calculerSurface()
+    {
+        return round(pi() * $this->rayon * $this->rayon, 3);
+    }
+}
+
+$cercle = new Cercle(10);
+$rectangle = new Rectangle(13,12);
+
+echo $cercle->calculerPerimetre() . '<br>';
+echo $cercle->calculerSurface() . ' m²<br>';
+echo $cercle->afficherInformations() . '<br>';
+
+echo $rectangle->calculerPerimetre() . '<br>';
+echo $rectangle->calculerSurface() . ' m²<br>';
+echo $rectangle->afficherInformations() . '<br>';
+
